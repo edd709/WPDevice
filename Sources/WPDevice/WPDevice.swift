88948eb937333cccc004da12f4c5951ecb2ece18
Erio Daniel Diaz <erio.daniel.diaz@gmail.com>
2022-09-09 15:11:15 -0300

    ..

--- Sources/WPDevice/WPDevice.swift
+++ Sources/WPDevice/WPDevice.swift
@@ -1,63 +1,70 @@
 public struct WPDevice {
-    
     public init() {}
-    
-    public struct Phone {
-        
-        public init() {}
-        
-        public struct name {
-            
-            public init() {}
-            
-            public var iPhone8 = "iPhone 8"
-            public var iPhone8Plus = "iPhone 8 Plus"
-            public var iPhone11 = "iPhone 11"
-            public var iPhone11Pro = "iPhone 11 Pro"
-            public var iPhone11ProMax = "iPhone 11 Pro Max"
-            public var iPhone12 = "iPhone 12"
-            public var iPhone12Pro = "iPhone 12 Pro"
-            public var iPhone12ProMax = "iPhone 12 Pro Max"
-            public var iPhone12Mini = "iPhone 12 mini"
-            public var iPhone13 = "iPhone 13"
-            public var iPhone13Pro = "iPhone 13 Pro"
-            public var iPhone13ProMax = "iPhone 13 Pro Max"
-            public var iPhone13Mini = "iPhone 13 mini"
-            public var iPhoneSE2nd = "iPhone SE (3rd generation)"
-        }
-        
-        public struct description {
-            
-            public init() {}
-            
-            public var iPhone8 = "iPhone8"
-            public var iPhone8Plus = "iPhone8Plus"
-            public var iPhone11 = "iPhone11"
-            public var iPhone11Pro = "iPhone11Pro"
-            public var iPhone11ProMax = "iPhone11ProMax"
-            public var iPhone12 = "iPhone12"
-            public var iPhone12Pro = "iPhone12Pro"
-            public var iPhone12ProMax = "iPhone12ProMax"
-            public var iPhone12Mini = "iPhone12mini"
-            public var iPhone13 = "iPhone13"
-            public var iPhone13Pro = "iPhone13Pro"
-            public var iPhone13ProMax = "iPhone13ProMax"
-            public var iPhone13Mini = "iPhone13mini"
-            public var iPhoneSE2nd = "iPhoneSE2020"
-        }
-        
-    }
-    
-    public struct Tablet {
-        
-        public init() {}
-        
-        static var iPadMini = "iPad mini (6th generation)"
-    }
-    public struct MediaPlayer {
-        
-        public init() {}
-        
-        static var iPodTouch7th = "iPod touch (7th generation)"
-    }
+    public var Item1 = "Item1"
+    public var Item2 = "Item2"
+    public var Item3 = "Item3"
 }
+
+//public struct WPDevice {
+//
+//    public init() {}
+//
+//    public struct Phone {
+//
+//        public init() {}
+//
+//        public struct name {
+//
+//            public init() {}
+//
+//            public var iPhone8 = "iPhone 8"
+//            public var iPhone8Plus = "iPhone 8 Plus"
+//            public var iPhone11 = "iPhone 11"
+//            public var iPhone11Pro = "iPhone 11 Pro"
+//            public var iPhone11ProMax = "iPhone 11 Pro Max"
+//            public var iPhone12 = "iPhone 12"
+//            public var iPhone12Pro = "iPhone 12 Pro"
+//            public var iPhone12ProMax = "iPhone 12 Pro Max"
+//            public var iPhone12Mini = "iPhone 12 mini"
+//            public var iPhone13 = "iPhone 13"
+//            public var iPhone13Pro = "iPhone 13 Pro"
+//            public var iPhone13ProMax = "iPhone 13 Pro Max"
+//            public var iPhone13Mini = "iPhone 13 mini"
+//            public var iPhoneSE2nd = "iPhone SE (3rd generation)"
+//        }
+//
+//        public struct description {
+//
+//            public init() {}
+//
+//            public var iPhone8 = "iPhone8"
+//            public var iPhone8Plus = "iPhone8Plus"
+//            public var iPhone11 = "iPhone11"
+//            public var iPhone11Pro = "iPhone11Pro"
+//            public var iPhone11ProMax = "iPhone11ProMax"
+//            public var iPhone12 = "iPhone12"
+//            public var iPhone12Pro = "iPhone12Pro"
+//            public var iPhone12ProMax = "iPhone12ProMax"
+//            public var iPhone12Mini = "iPhone12mini"
+//            public var iPhone13 = "iPhone13"
+//            public var iPhone13Pro = "iPhone13Pro"
+//            public var iPhone13ProMax = "iPhone13ProMax"
+//            public var iPhone13Mini = "iPhone13mini"
+//            public var iPhoneSE2nd = "iPhoneSE2020"
+//        }
+//
+//    }
+//
+//    public struct Tablet {
+//
+//        public init() {}
+//
+//        static var iPadMini = "iPad mini (6th generation)"
+//    }
+//    public struct MediaPlayer {
+//
+//        public init() {}
+//
+//        static var iPodTouch7th = "iPod touch (7th generation)"
+//    }
+//}
