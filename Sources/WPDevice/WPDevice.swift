2bd674c5583fe19d22ac62f32d5ce8500fd65979
Erio Daniel Diaz <erio.daniel.diaz@gmail.com>
2022-09-09 12:24:05 -0300

    first commit

---
+++ Sources/WPDevice/WPDevice.swift
@@ -0,0 +1,45 @@
+public struct WPDevice {
+    public struct Phone {
+        public struct name {
+            static var iPhone8 = "iPhone 8"
+            static var iPhone8Plus = "iPhone 8 Plus"
+            static var iPhone11 = "iPhone 11"
+            static var iPhone11Pro = "iPhone 11 Pro"
+            static var iPhone11ProMax = "iPhone 11 Pro Max"
+            static var iPhone12 = "iPhone 12"
+            static var iPhone12Pro = "iPhone 12 Pro"
+            static var iPhone12ProMax = "iPhone 12 Pro Max"
+            static var iPhone12Mini = "iPhone 12 mini"
+            static var iPhone13 = "iPhone 13"
+            static var iPhone13Pro = "iPhone 13 Pro"
+            static var iPhone13ProMax = "iPhone 13 Pro Max"
+            static var iPhone13Mini = "iPhone 13 mini"
+            static var iPhoneSE2nd = "iPhone SE (3rd generation)"
+        }
+        
+        public struct description {
+            static var iPhone8 = "iPhone8"
+            static var iPhone8Plus = "iPhone8Plus"
+            static var iPhone11 = "iPhone11"
+            static var iPhone11Pro = "iPhone11Pro"
+            static var iPhone11ProMax = "iPhone11ProMax"
+            static var iPhone12 = "iPhone12"
+            static var iPhone12Pro = "iPhone12Pro"
+            static var iPhone12ProMax = "iPhone12ProMax"
+            static var iPhone12Mini = "iPhone12mini"
+            static var iPhone13 = "iPhone13"
+            static var iPhone13Pro = "iPhone13Pro"
+            static var iPhone13ProMax = "iPhone13ProMax"
+            static var iPhone13Mini = "iPhone13mini"
+            static var iPhoneSE2nd = "iPhoneSE2020"
+        }
+        
+    }
+    
+    public struct Tablet {
+        static var iPadMini = "iPad mini (6th generation)"
+    }
+    public struct MediaPlayer {
+        static var iPodTouch7th = "iPod touch (7th generation)"
+    }
+}
---
+++ Tests/WPDeviceTests/WPDeviceTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import WPDevice
+
+final class WPDeviceTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(WPDevice().text, "Hello, World!")
+    }
+}
